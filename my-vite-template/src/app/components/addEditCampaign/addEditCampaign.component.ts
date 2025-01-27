5b0783f090f9a89389a3431d28f51a45a55681ea
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2025-01-27 13:51:07 +0100

    dialog wip

--- my-vite-template/src/app/components/addEditCampaign/addEditCampaign.component.ts
+++ my-vite-template/src/app/components/addEditCampaign/addEditCampaign.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, model } from '@angular/core';
+import { Component, computed, inject, model, signal } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -10,16 +12,33 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import {
+  MatAutocompleteModule,
+  MatAutocompleteSelectedEvent,
+} from '@angular/material/autocomplete';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {
+  MatSlideToggleModule,
+  _MatSlideToggleRequiredValidatorModule,
+} from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
 
 export interface DialogData {
   animal: string;
   name: string;
 }
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'dialog',
   templateUrl: 'addEditCampaign.html',
+  styleUrl: 'addEditCampaign.css',
   imports: [
     MatFormFieldModule,
     MatInputModule,
@@ -29,12 +48,75 @@ export interface DialogData {
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    MatChipsModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule,
+    MatSelectModule,
   ],
 })
 export class AddEditCampaign {
   readonly dialogRef = inject(MatDialogRef<AddEditCampaign>);
   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
   readonly animal = model(this.data.animal);
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  readonly currentFruit = model('');
+  readonly fruits = signal(['Lemon']);
+  readonly allFruits: string[] = [
+    'Apple',
+    'Lemon',
+    'Lime',
+    'Orange',
+    'Strawberry',
+  ];
+  selectedValue: string | undefined;
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
+
+  readonly filteredFruits = computed(() => {
+    const currentFruit = this.currentFruit().toLowerCase();
+    return currentFruit
+      ? this.allFruits.filter((fruit) =>
+          fruit.toLowerCase().includes(currentFruit)
+        )
+      : this.allFruits.slice();
+  });
+
+  readonly announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.update((fruits) => [...fruits, value]);
+    }
+
+    // Clear the input value
+    this.currentFruit.set('');
+  }
+
+  remove(fruit: string): void {
+    this.fruits.update((fruits) => {
+      const index = fruits.indexOf(fruit);
+      if (index < 0) {
+        return fruits;
+      }
+
+      fruits.splice(index, 1);
+      this.announcer.announce(`Removed ${fruit}`);
+      return [...fruits];
+    });
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.fruits.update((fruits) => [...fruits, event.option.viewValue]);
+    this.currentFruit.set('');
+    event.option.deselect();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
